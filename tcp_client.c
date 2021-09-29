5e9ff9a7bb59008702bcb9405c71a1327de37647
William Xu <79387306+WilliamXu50@users.noreply.github.com>
2021-09-28 21:09:27 -0400

    Update tcp_client.c

--- tcp_client.c
+++ tcp_client.c
@@ -31,153 +31,154 @@ void send_image(int sockfd);
 void receive_image();
 
 int main(){
-		printf("\n=========================================\n");
-		printf("\n           Welcome to Chat Room\n");
-		printf("\n=========================================\n");
+    printf("\n=========================================\n");
+    printf("\n           Welcome to Chat Room\n");
+    printf("\n=========================================\n");
 
-		int socket_fd;
-		printf("\nEnter your name:\n");
-		//Gets the user's name
-		fgets(name, NAME_LENGTH, stdin);
-		trim_str(name);
+    int socket_fd;
+    printf("\nEnter your name:\n");
+    //Gets the user's name
+    fgets(name, NAME_LENGTH, stdin);
+    trim_str(name);
 
-		//Create a client socket using socket()
-		socket_fd=socket(AF_INET, SOCK_STREAM, 0);
+    //Create a client socket using socket()
+    socket_fd=socket(AF_INET, SOCK_STREAM, 0);
 
-		//If socket creation failed
-		if(socket_fd==-1){
-				perror("Socket Error: ");		//Print error message
-				exit(1);		//Exit
-		}
+    //If socket creation failed
+    if(socket_fd==-1){
+	perror("Socket Error: ");		//Print error message
+        exit(1);		//Exit
+    }
 
-		//Create an address for the client to connect to
-		//For us, it will be the server's address
-		struct sockaddr_in server_addr;
-		memset(&server_addr, '\0', sizeof(server_addr));
+    //Create an address for the client to connect to
+    //For us, it will be the server's address
+    struct sockaddr_in server_addr;
+    memset(&server_addr, '\0', sizeof(server_addr));
 
-		server_addr.sin_family=AF_INET;		//Address family
-		server_addr.sin_port=htons(PORT);		//Port number
-		server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");		//IP address: local machine
+    server_addr.sin_family=AF_INET;		//Address family
+    server_addr.sin_port=htons(PORT);		//Port number
+    server_addr.sin_addr.s_addr=inet_addr("127.0.0.1");		//IP address: local machine
 
-		//Connect client socket to the server address
-		int connect_status;
-		connect_status=connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    //Connect client socket to the server address
+    int connect_status;
+    connect_status=connect(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
 
-		if(connect_status==0){
-				printf("\nSuccessfully connected to server!\n");
-		}
-		else{
-				perror("Connection Error: ");		//Print error message
-				close(socket_fd);			//Close socket
-				exit(1);		//Exit
-		}
+    if(connect_status==0){
+	printf("\nSuccessfully connected to server!\n");
+    }
+    else{
+	perror("Connection Error: ");		//Print error message
+	close(socket_fd);			//Close socket
+	exit(1);		//Exit
+    }
 
-		send(socket_fd, name, NAME_LENGTH, 0);	//Send name to server
+    send(socket_fd, name, NAME_LENGTH, 0);	//Send name to server
 
-		signal(SIGINT, signal_handler);			//Deals with ctr-C kill signal
+    signal(SIGINT, signal_handler);			//Deals with ctr-C kill signal
 
-		//Create a thread for sending messages
-		pthread_t send_msg_thread;
-		int send_msg_thread_status;
-		send_msg_thread_status=pthread_create(&send_msg_thread, NULL, &client_send_msg, (void *)&socket_fd);
-		if(send_msg_thread_status!=0){
-				perror("Error while creating thread: ");
-				exit(1);
-		}
+    //Create a thread for sending messages
+    pthread_t send_msg_thread;
+    int send_msg_thread_status;
+    send_msg_thread_status=pthread_create(&send_msg_thread, NULL, &client_send_msg, (void *)&socket_fd);
+        
+    if(send_msg_thread_status!=0){
+	perror("Error while creating thread: ");
+	exit(1);
+    }
 
-		//Create a thread for receiving messages
-		pthread_t recv_msg_thread;
-		int recv_msg_thread_status;
-		recv_msg_thread_status=pthread_create(&recv_msg_thread, NULL, &client_recv_msg, (void *)&socket_fd);
-		if(recv_msg_thread_status!=0){
-				perror("Error while creating thread: ");
-				exit(1);
-		}
+    //Create a thread for receiving messages
+    pthread_t recv_msg_thread;
+    int recv_msg_thread_status;
+    recv_msg_thread_status=pthread_create(&recv_msg_thread, NULL, &client_recv_msg, (void *)&socket_fd);
+    if(recv_msg_thread_status!=0){
+	perror("Error while creating thread: ");
+	exit(1);
+    }
 
-		while(1){
-				//Exit if either flag is set
-				if(signal_flag!=0 || leave_flag!=0){
-						break;
-				}
-		}
+    while(1){
+        //Exit if either flag is set
+        if(signal_flag!=0 || leave_flag!=0){
+	    break;
+        }
+    }
 
-		close(socket_fd);		//Close socket
+    close(socket_fd);		//Close socket
 
-		return 0;
+    return 0;
 }
 
 void signal_handler(int sig){
-		signal_flag=1;
-		printf("Exiting chat room...\n");
+    signal_flag=1;
+    printf("Exiting chat room...\n");
 }
 
 void flush_stdout(){
-		printf("%s",">");
-		fflush(stdout);
+    printf("%s",">");
+    fflush(stdout);
 }
 
 //Replace the '\n' at the end of a string with a null terminator '\0'
 void trim_str(char* str){
-		if(str[strlen(str)-1]=='\n'){
-				str[strlen(str)-1]='\0';
-		}
+    if(str[strlen(str)-1]=='\n'){
+    str[strlen(str)-1]='\0';
+    }
 }
 
 //Thread for sending messages
 void *client_send_msg(void *arg){
-		int socket_fd=*((int *)arg);
-		char message_buffer[MAX]={};
-		char send_buffer[NAME_LENGTH+MAX];
-		char file_name[MAX];
+    int socket_fd=*((int *)arg);
+    char message_buffer[MAX]={};
+    char send_buffer[NAME_LENGTH+MAX];
+    char file_name[MAX];
 
-		while(1){
-				flush_stdout();
-				fgets(message_buffer, MAX, stdin);
-				trim_str(message_buffer);
+    while(1){
+	flush_stdout();
+	fgets(message_buffer, MAX, stdin);
+	trim_str(message_buffer);
 
 
-				if(strcmp(message_buffer, "exit")==0){
-						sprintf(send_buffer, "%s has left the chat\n", name);
-						//Notify server that the client has left the chat
-						send(socket_fd, send_buffer, (NAME_LENGTH+MAX), 0);
-						printf("Exiting chatroom...\n");
-						leave_flag=1;
-						break;
-						}
-				else{
-						//Message will be sent in the following format:
-						//Name: *message
-						sprintf(send_buffer, "%s: %s", name, message_buffer);
-						send(socket_fd, send_buffer, strlen(send_buffer), 0);
-				}
+	if(strcmp(message_buffer, "exit")==0){
+	    sprintf(send_buffer, "%s has left the chat\n", name);
+	    //Notify server that the client has left the chat
+	    send(socket_fd, send_buffer, (NAME_LENGTH+MAX), 0);
+	    printf("Exiting chatroom...\n");
+	    leave_flag=1;
+	    break;
+	}
+	else{
+	    //Message will be sent in the following format:
+	    //Name: *message
+	    sprintf(send_buffer, "%s: %s", name, message_buffer);
+	    send(socket_fd, send_buffer, strlen(send_buffer), 0);
+	}
 
-				//Clear the buffers
-				memset(message_buffer, '\0', MAX);
-				memset(send_buffer, '\0', (NAME_LENGTH+MAX));
-		}
+	//Clear the buffers
+	memset(message_buffer, '\0', MAX);
+	memset(send_buffer, '\0', (NAME_LENGTH+MAX));
+	}
 }
 
 //Thread for receiving messages
 void *client_recv_msg(void *arg){
-		int socket_fd=*((int *)arg);
-		char recv_msg_buffer[NAME_LENGTH+MAX];
+    int socket_fd=*((int *)arg);
+    char recv_msg_buffer[NAME_LENGTH+MAX];
 
-		while(1){
-				int recv_status;
-				recv_status=recv(socket_fd, recv_msg_buffer, (NAME_LENGTH+MAX), 0);
+    while(1){
+        int recv_status;
+        recv_status=recv(socket_fd, recv_msg_buffer, (NAME_LENGTH+MAX), 0);
 
-				if(recv_status>0){
-						printf("%s\n", recv_msg_buffer);
-						flush_stdout();
-				}
-				else if(recv_status==0){
-						break;
-				}
-				else{
-						perror("Error in receiving message: ");
-						break;
-				}
-				//Clear buffer
-				memset(recv_msg_buffer, '\0', (NAME_LENGTH+MAX));
-		}
+        if(recv_status>0){
+            printf("%s\n", recv_msg_buffer);
+            flush_stdout();
+        }
+        else if(recv_status==0){
+	    break;
+        }
+        else{
+	    perror("Error in receiving message: ");
+	    break;
+        }
+	//Clear buffer
+	memset(recv_msg_buffer, '\0', (NAME_LENGTH+MAX));
+	}
 }
